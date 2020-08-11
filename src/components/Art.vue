cc9f862d0648426b5b6f81f55d117a28e6ff774d
KSiotis <siotiskostas@gmail.com>
2020-08-11 13:36:43 +0300

    login modal, ready for usability changes

--- src/components/Art.vue
+++ src/components/Art.vue
@@ -11,11 +11,7 @@
 				<span class="ltext">ΕΛ</span>
 			</a>
 
-			<button
-				v-if="!user"
-				class="d-flex align-items-center"
-				v-on:click="$router.push('/login').catch(() => {})"
-			>
+			<button v-if="!user" v-b-modal.login-modal class="d-flex align-items-center">
 				<i class="iconify mr-1" data-icon="ion-person-circle-outline" />
 				Σύνδεση / Εγγραφή
 			</button>
@@ -28,20 +24,61 @@
 				{{user.fullName}} / Προφίλ
 			</button>
 		</div>
+
+		<b-modal id="login-modal" title="BootstrapVue">
+			<template v-slot:modal-header="{close}">
+				<div class="widen d-flex">
+
+					<span id="to-register" class="mr-auto mt-3">
+						Δεν έχω λογαριασμό.
+						<a id="register-link" v-on:click="$router.push('/register').catch(() => {})" href="">Εγγραφή</a>
+					</span>
+
+					<a @click="close()">
+						<span class="iconify" data-icon="ion-close-circle-outline"></span>
+					</a>
+
+				</div>
+			</template>
+
+			<b-form @submit="onSubmit" class="mt-3">
+				<b-form-input id="modal-input-top" v-model="form.username" placeholder="Όνομα Χρήστη" size="lg"></b-form-input>
+				<b-form-input id="modal-input-bot" v-model="form.password" type="password" placeholder="Κωδικός Πρόσβασης" size="lg"></b-form-input>
+				<b-button id="login-btn" type="submit" class="widen mt-5">Είσοδος</b-button>
+			</b-form>
+
+			<template v-slot:modal-footer="" class="d-flex justify-content-start widen">
+				<div class="d-flex flex-column links-container widen">
+					<a href="">Ξέχασα τον κωδικό μου</a>
+					<a href="">Ξέχασα το όνομα χρήστη</a>
+				</div>
+			</template>
+
+		</b-modal>
 	</div>
 </template>
 
 <script>
 export default {
 	data() {
-		return {};
+		return {
+			form: {
+				username: '',
+				password: ''
+			}
+		};
 	},
 	computed: {
 		user() {
 			return this.$store.state.user;
 		},
 	},
-	methods: {},
+	methods: {
+		onSubmit(evt) {
+			evt.preventDefault()
+			alert(JSON.stringify(this.form))
+		},
+	},
 };
 </script>
 
@@ -118,5 +155,48 @@ button:active {
 	color: white;
 }
 
+.widen {
+	width: 100%;
+}
+
+.widen a {
+	width: fit-content;
+	color: #194A50;
+}
+
+#to-register {
+	color: #194A50;
+}
+
+#register-link {
+	color: #D37556;
+}
+
+.links-container a {
+	color: #194A50;
+}
+
+#login-btn {
+	border-bottom-left-radius: 10px;
+	border-bottom-right-radius: 10px;
+	border-top-left-radius: 10px;
+	border-top-right-radius: 10px;
+	background-color: #D37556;
+	color: white;
+}
+
+#login-btn:hover {
+	background-color: #9C533B;
+}
+
+#modal-input-top {
+	border-bottom-left-radius: 0px;
+	border-bottom-right-radius: 0px;
+}
+
+#modal-input-bot {
+	border-top-left-radius: 0px;
+	border-top-right-radius: 0px;
+}
 
 </style>
