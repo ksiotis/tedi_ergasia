9ab97f5660a4904c9c743af00c8088011e8d20c7
Thomas Papathimiopoulos <thomaspapathimiopoylos@gmail.com>
2020-08-17 22:03:27 +0300

    added  more to accommodation view

--- src/views/Accommodation.vue
+++ src/views/Accommodation.vue
@@ -1,53 +1,106 @@
 <template>
-    <div class="d-flex flex-column justify-content-start container">
-        <!-- τίτλος  -->
-        <h2 class="title">Τίτλος χώρου</h2>
-        <!-- εικόνες -->
-        <div class="element">
-            <b-carousel
-            id="carousel-1"
-            v-model="slide"
-            :interval="3000"
-            controls
-            indicators
-            background="#ccc"
-            img-width="1024"
-            img-height="480"
-            style="text-shadow: 1px 1px 2px #000;"
-            @sliding-start="onSlideStart"
-            @sliding-end="onSlideEnd"
-            >
-                <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=52"/>
-                <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=53"/>
-                <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54"/>
-                <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=55"/>
-           
-            </b-carousel>
-        </div>
-        <!-- περιγραφή -->
-        <h3 class="subtitle element">Περιγραφή</h3>
-        <p> {{placeholderText}} </p>
-        
-        <!-- χαρακτηρηστικά -->
-        <h3 class="subtitle element">Χαρακτηρηστικά</h3>
-        <div class="d-flex flex-row align-items-center">
-            <p class="area">{{area}}</p>
-            <p>/τ.μ.</p>
-        </div>
-        <form 
-            class="d-flex flex-column"
-            v-for="(characteristic, index) in characteristics"
-			v-bind:key="index">
-                <div 
-                    class="d-flex flex-row align-items-center justify-content-between characterisitc"
-                    v-if="characteristic.status"
-                    >
-                        <span>{{ characteristic.name }}</span>
-                        <i aria-hidden="true" class="iconify mr-2" v-bind:data-icon="characteristic.icon" />
-                </div>
-        </form>
+    <div class = "d-flex flex-column justify-content-center container"> 
+        <div class="d-flex flex-column justify-content-start">
+            <!-- τίτλος  -->
+            <h2 class="title">Τίτλος χώρου</h2>
+            <!-- εικόνες -->
+            <div class="element">
+                <b-carousel
+                id="carousel-1"
+                v-model="slide"
+                :interval="3000"
+                controls
+                indicators
+                background="#ccc"
+                img-width="1024"
+                img-height="480"
+                style="text-shadow: 1px 1px 2px #000;"
+                @sliding-start="onSlideStart"
+                @sliding-end="onSlideEnd"
+                >
+                    <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=52"/>
+                    <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=53"/>
+                    <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54"/>
+                    <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=55"/>
+            
+                </b-carousel>
+            </div>
+            <!-- περιγραφή -->
+            <h3 class="subtitle element">Περιγραφή</h3>
+            <p> {{placeholderText}} </p>
+            
+            <!-- χαρακτηρηστικά -->
+            <div class="d-flex flex-row justify-content-between">
+                <!-- στήλη 1 -->
+                <div>
+                    <h3 class="subtitle element">Χαρακτηρηστικά</h3>
+                    <div class="d-flex flex-row align-items-center">
+                        <p class="area">{{area}}</p>
+                        <p>/τ.μ.</p>
+                    </div>
 
-        
+                    <div class="characteristicsContainer">
+                        <form 
+                            class="d-flex flex-column"
+                            v-for="(characteristic, index) in characteristics"
+                            v-bind:key="index">
+                                <div 
+                                    class="d-flex flex-row align-items-center justify-content-between characterisitc"
+                                    v-if="characteristic.status"
+                                    >
+                                        <span>{{ characteristic.name }}</span>
+                                        <i aria-hidden="true" class="iconify mr-2" v-bind:data-icon="characteristic.icon" />
+                                </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- στήλη 2 -->
+                <div>
+
+                    <h3 class="subtitle element">Ελάχιστη τιμή</h3>
+                    <div class="d-flex flex-row align-items-center">
+                        <p class="area">{{price}}</p>
+                        <p class="area">€</p>
+                        <p>/διανυκτέρευση</p>
+                    </div>
+
+                    <h3 class="subtitle element">Επιπλέον κόστος</h3>
+                    <div class="d-flex flex-row align-items-center">
+                        <p class="area">{{extraCost}}</p>
+                        <p class="area">€</p>
+                        <p>/ανά άτομο</p>
+                    </div>
+
+                    <!-- αριθμητικά -->
+                    <div class="d-flex flex-row justify-content-between arithmetic">
+                        <p>Ελάχιστες μέρες:</p>
+                        <p>{{minDays}}</p>
+                    </div>
+                    <div class="d-flex flex-row justify-content-between arithmetic">
+                        <p>Αριθμός ατόμων:</p>
+                        <p>{{numPersons}}</p>
+                    </div>
+                    <div class="d-flex flex-row justify-content-between arithmetic">
+                        <p>Αριθμός κρεβατιών:</p>
+                        <p>{{numBeds}}</p>
+                    </div>
+                    <div class="d-flex flex-row justify-content-between arithmetic">
+                        <p>Αριθμός μπάνιων:</p>
+                        <p>{{numBaths}}</p>
+                    </div>
+                    <div class="d-flex flex-row justify-content-between arithmetic">
+                        <p>Αριθμός κρεβατιών:</p>
+                        <p>{{numBedrooms}}</p>
+                    </div>
+                    <div class="d-flex flex-row justify-content-between arithmetic">
+                        <p>Αριθμός υπνοδωματίων:</p>
+                        <p>{{numBedrooms}}</p>
+                    </div>
+                    
+                </div>
+            </div>
+            
+        </div>
     </div>
 </template>
 
@@ -59,8 +112,18 @@
         return {
             slide: 0,
             sliding: null,
+            
             placeholderText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quam sapien, rutrum viverra nunc et, tincidunt sagittis libero. Curabitur dictum tellus sit amet arcu ultrices, in accumsan ipsum ultricies. Donec blandit ac felis id varius. Vestibulum quam tortor, ullamcorper ut euismod et, feugiat et nisl.',
+            
             area: "46",
+            price: "68",
+            extraCost: "55",
+            
+            minDays: "5",
+            numPersons: "3",
+            numBeds: "3",
+            numBaths: "4",
+            numBedrooms: "1",
 
             characteristics: [
                 {
@@ -119,7 +182,7 @@
 
 <style scoped>
 .container{
-    margin-left: 125px;
+    padding: 0 100px;
     margin-top: 50px;
 }
 
@@ -167,9 +230,26 @@
     font-size: 18px;
     line-height: 21px;
 
-    width: 25%;
+    width: 300px;
 
     color: #4E7378;
+}
+
+.arithmetic{
+    font-family: Roboto;
+    font-style: normal;
+    font-size: 18px;
+    line-height: 21px;
+    
+    width: 300px;
+
+    margin-bottom: -10px;
+
+    color: #4E7378;
+}
+
+.characteristicsContainer{
+
 }
 
 </style>
