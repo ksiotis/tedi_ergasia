4f7f768f99aad01581b24b83cdd4267263995c03
Thomas Papathimiopoulos <thomaspapathimiopoylos@gmail.com>
2020-08-17 21:30:35 +0300

    added dynamic characteristics list in accomodation

--- src/views/Accommodation.vue
+++ src/views/Accommodation.vue
@@ -3,9 +3,7 @@
         <!-- τίτλος  -->
         <h2 class="title">Τίτλος χώρου</h2>
         <!-- εικόνες -->
-        
-
-        <div>
+        <div class="element">
             <b-carousel
             id="carousel-1"
             v-model="slide"
@@ -26,6 +24,30 @@
            
             </b-carousel>
         </div>
+        <!-- περιγραφή -->
+        <h3 class="subtitle element">Περιγραφή</h3>
+        <p> {{placeholderText}} </p>
+        
+        <!-- χαρακτηρηστικά -->
+        <h3 class="subtitle element">Χαρακτηρηστικά</h3>
+        <div class="d-flex flex-row align-items-center">
+            <p class="area">{{area}}</p>
+            <p>/τ.μ.</p>
+        </div>
+        <form 
+            class="d-flex flex-column"
+            v-for="(characteristic, index) in characteristics"
+			v-bind:key="index">
+                <div 
+                    class="d-flex flex-row align-items-center justify-content-between characterisitc"
+                    v-if="characteristic.status"
+                    >
+                        <span>{{ characteristic.name }}</span>
+                        <i aria-hidden="true" class="iconify mr-2" v-bind:data-icon="characteristic.icon" />
+                </div>
+        </form>
+
+        
     </div>
 </template>
 
@@ -34,10 +56,55 @@
 <script>
  export default {
     data() {
-      return {
-        slide: 0,
-        sliding: null
-      }
+        return {
+            slide: 0,
+            sliding: null,
+            placeholderText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quam sapien, rutrum viverra nunc et, tincidunt sagittis libero. Curabitur dictum tellus sit amet arcu ultrices, in accumsan ipsum ultricies. Donec blandit ac felis id varius. Vestibulum quam tortor, ullamcorper ut euismod et, feugiat et nisl.',
+            area: "46",
+
+            characteristics: [
+                {
+                    name: 'Wifi',
+                    icon: 'ion-wifi',
+                    status: true,
+                },
+                {
+                    name: 'Ψύξη',
+                    icon: 'ion-snow',
+                    status: true,
+                },
+                {
+                    name: 'Θέρμανση',
+                    icon: 'ion-thermometer',
+                    status: true,
+                },
+                {
+                    name: 'Κουζίνα',
+                    icon: 'ion-fast-food',
+                    status: true,
+                },
+                {
+                    name: 'Τηλεόραση',
+                    icon: 'ion-tv',
+                    status: true,
+                },
+                {
+                    name: 'Χώρος στάθμευσης',
+                    icon: 'ion-car',
+                    status: true,
+                },
+                {
+                    name: 'Ανελκυστήρας',
+                    icon: 'ion-arrow-up',
+                    status: true,
+                },
+                {
+                    name: 'Καθιστικό',
+                    icon: 'ion-happy-outline',
+                    status: true,
+                },
+            ]
+        }
     },
     methods: {
       onSlideStart() {
@@ -56,17 +123,53 @@
     margin-top: 50px;
 }
 
-.title{
+.element{
+    margin-top: 25px;
+    margin-bottom: 10px;
+}
 
+.title{
+    font-family: Roboto;
     font-style: normal;
-    font-weight: 900;
-    line-height: 56px;
+    font-weight: bold;
+    font-size: 36px;
+    line-height: 42px;
 
     color: #194A50;
 
 }
 
-img{
-    width: 100px;
+.subtitle{
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 28px;
+
+    color: #194A50;
 }
+
+.area{
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 28px;
+    text-align: justify;
+
+    color: #484A4A;
+}
+
+.characterisitc{
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 21px;
+
+    width: 25%;
+
+    color: #4E7378;
+}
+
 </style>
