5c303318b14fcb4226c8f0326c960f3731f759e1
KSiotis <siotiskostas@gmail.com>
2020-08-18 19:04:29 +0300

    very ugly modal, TODO turn into its own page

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -8,8 +8,8 @@
             <div id="mail-buttons" class="d-flex mt-2">
                 <button id="incoming" class="mr-2">Εισερχόμενα</button>
                 <button id="outgoing" class=" mr-auto">Εξερχόμενα</button>
-                <button id="new-mail" class="d-flex">
-                    <span class="mr-2">Νέο Μήνυμα</span>
+                <button v-b-modal.new-message-modal id="new-mail" class="d-flex">
+                    <span class="mr-2" >Νέο Μήνυμα</span>
                     <span class="iconify" data-icon="ion-mail-unread-outline" />
                 </button>
             </div>
@@ -28,6 +28,49 @@
             </tr>
             </table>
         </div>
+
+        <b-modal id="new-message-modal" title="BootstrapVue">
+			<template v-slot:modal-header="{close}">
+				<div class="widen d-flex">
+
+					<span id="to-register" class="mr-auto mt-3">
+						<span class="mr-2">Νέο Μήνυμα</span>
+                        <span class="iconify" data-icon="ion-mail-unread-outline" />
+					</span>
+
+					<a @click="close()">
+						<span class="iconify" data-icon="ion-close-circle-outline"></span>
+					</a>
+
+				</div>
+			</template>
+
+			<b-form @submit="send" class="mt-3">
+                <b-form-group label-cols="2" label="Από:" label-for="modal-username">
+				    <b-form-input id="modal-username" v-model="user.username" required disabled/>
+                </b-form-group>
+
+                <b-form-group label-cols="2" label="Προς:" label-for="modal-input-top">
+				    <b-form-input id="modal-input-top" v-model="form.to" required/>
+                </b-form-group>
+
+                <b-form-group label-cols="2" label="Θέμα:" label-for="modal-input-to">
+    				<b-form-input id="modal-input-to" v-model="form.subject" required/>
+                </b-form-group>
+
+                <b-form-group label-cols="2" label="Μήνυμα:" label-for="textarea-large">
+				    <b-form-textarea id="textarea-large" v-model="form.message" required/>
+                </b-form-group>
+			</b-form>
+
+			<template v-slot:modal-footer="" class="d-flex justify-content-start widen">
+				<button id="send-button" class="d-flex">
+                    <span >Αποστολή</span>
+                    <span class="iconify" data-icon="ion-send"/>
+                </button>
+			</template>
+
+		</b-modal>
     </div>
 </template>
 
@@ -36,6 +79,9 @@ export default {
     name: "Messages",
     data() {
         return {
+            user: {
+                username: "quirkygirl85"
+            },
             mymessages: [
                 {
                     subject: "why coronavirus is a semetic lie and how to protect yourself from it",
@@ -48,8 +94,18 @@ export default {
                     when: "22/1/1999",
                 }
             ],
+            form: {
+                to: "",
+                subject: "",
+                message: "",
+            }
         }
     },
+    methods: {
+        send() {
+
+        },
+    }
 }
 </script>
 
@@ -146,6 +202,29 @@ th {
     text-align: center;
     padding: 8px;
     border: #194A50 solid 1px;
+}
+
+.widen {
+	width: 100%;
+}
+
+#new-message-modal span {
+    font-size: 28px;
+}
+
+#new-message-modal .iconify {
+	height: 32px;
+	width: 32px;
+	color: #153D41;
+}
+
+#send-button {
+    font-size: 12px;
+    border: none;
+    background-color: #D37556;
+    color: white;
+    border-radius: 90px;
+    padding: 1px 20px;
 }
 
 </style>
